ea6728f4ed23b8d1696b7c83f466d9d2ec02ee70
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-10-04 18:28:13 -0500

    finished part a

--- lab05/lab05a/MainClass.java
+++ lab05/lab05a/MainClass.java
@@ -7,7 +7,28 @@ package lab05.lab05a;
 public class MainClass {
 
     public static void main(String[] args) {
+       //int[] inputArray = new int[10];
+       int[] inputArray = {1, 2, 3, 4, 5};
+       //int key = 2;
+       
+       int key = mySearch(inputArray, 3);
+        System.out.println(key);
         
-        
+        key = mySearch(inputArray, 0);
+        System.out.println(key);
+    }
+
+    public static int mySearch(int[] inputArray, int n)
+
+    {
+
+        for(int i = 0; i < inputArray.length; i++)
+        {
+            if(inputArray[i] == n)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
