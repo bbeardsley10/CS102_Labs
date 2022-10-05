28cd8ffe54b9195d3b8cf2275445521dbc0f0a05
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-10-04 20:36:22 -0500

    Finished compilation error portion

---
+++ lab06/lab06a/mainClass.java
@@ -0,0 +1,19 @@
+package lab06.lab06a;
+
+public class mainClass {
+    
+    /* Compilation Error Exercise */
+
+    public static void main(String[] args) {
+        
+        int a = 5, b = 10;
+
+        int Sum =  a + b;
+
+        //System.out.println("Sum of variables is " + sum); // Won't print anything because we forgot to capitalize the s in sum
+        
+        //Correct Solution
+        System.out.println("Sume of variables is " + Sum);
+
+    }
+}
