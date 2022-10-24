4cc8cb8f961f6689aa7854aab5c3d672cc399ab8
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-10-24 14:42:24 -0500

    Updated gitignore and finished Lab08

--- lab08/Rectangle.java
+++ lab08/Rectangle.java
@@ -6,17 +6,40 @@ public class Rectangle {
   int height;
   boolean filled;
 
-  public Rectangle(int width, int height)
+  public Rectangle(int width, int height, boolean filled)
   {
     this.width = width;
     this.height = height;
-
+    this.filled = filled;
   }
 
   
   public void draw()
   {
-      for(int i = 0; i< height; i++)
+    /*
+     * This is the implementation for the extra credit for creating a rectangle that is unfilled
+     */
+    if(filled == false)
+    {
+      for(int i = 0; i < height; i++)
+      {
+        for(int j = 0; j < width; j++)
+        {
+          if(i == 0 || i == height-1 || j == 0 || j == width-1)
+            {
+              System.out.print("*");
+            } else{
+              System.out.print(" ");
+            }
+        }
+        System.out.println();
+      }
+    }
+    /*
+     * This is the default filled rectangle method
+     */
+    else{
+      for(int i = 0; i < height; i++)
       {
         for(int j = 0; j < width; j++)
         {
@@ -24,5 +47,7 @@ public class Rectangle {
         }
         System.out.println("");
       }
+    }
   }
 }
+
--- lab08/mainClass.java
+++ lab08/mainClass.java
@@ -1,14 +1,17 @@
 package lab08;
 
 public class mainClass {
-  
+
   public static void main(String[] args) {
     
-    Rectangle r1 = new Rectangle(5, 8);
+    Rectangle r1 = new Rectangle(5, 8, true);
     r1.draw();
 
-    Rectangle r2 = new Rectangle(8, 5);
+    Rectangle r2 = new Rectangle(8, 5, false);
     r2.draw();
+
+    Rectangle r3 = new Rectangle(3, 3, false);
+    r3.draw();
   }
 
 }
