350a872923cf389f2bb4a362b008a6a4e9f1edb1
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-11-15 11:38:50 -0600

    Updated lab10

--- lab10/mainClass.java
+++ lab10/mainClass.java
@@ -1,9 +1,36 @@
 package lab10;
 
+import java.util.Stack;
+
 public class mainClass {
   
-
   public static void main(String[] args) {
-    
+
+    System.out.println(isBalanced("(2+4)*8+((3-1)+7)"));
+    System.out.println(isBalanced("(2+4*7 + (3-1) + 8"));
+    System.out.println(isBalanced("(3*4)+((4+5)/4)"));
+  }
+
+  public static Boolean isBalanced(String expression) {
+    char[] exp = expression.toCharArray();
+    Stack<Character> stack = new Stack();
+
+    for(int i = 0; i < exp.length; i++) {
+      if(exp[i] == '('){
+        stack.push('(');
+      }
+      if(exp[i] == ')'){
+        if(stack.isEmpty()){
+          return false;
+        }
+        stack.pop();
+      }
+    }
+
+    if(stack.isEmpty()){
+      return true;
+    }
+
+    return false;
   }
 }
